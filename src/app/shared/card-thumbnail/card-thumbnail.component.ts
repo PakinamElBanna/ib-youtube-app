945775a75dc05a5881f229bbb8bf1c2cecd994d4
Pakinam El Banna <pakinam_banna@hotmail.com>
2019-02-02 23:49:33 +0200

    Setup basic generic card component for list display

---
+++ src/app/shared/card-thumbnail/card-thumbnail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-thumbnail',
+  templateUrl: './card-thumbnail.component.html',
+  styleUrls: ['./card-thumbnail.component.scss']
+})
+export class CardThumbnailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,14 +4,30 @@ import { ChannelCardComponent } from './channel-card/channel-card.component';
 import { VideoCardComponent } from './video-card/video-card.component';
 import { PlaylistCardComponent } from './playlist-card/playlist-card.component';
 import { LoadingComponent } from './loading/loading.component';
+import { AvatarComponent } from './avatar/avatar.component';
+import { InfoCardComponent } from './info-card/info-card.component';
+import { CardThumbnailComponent } from './card-thumbnail/card-thumbnail.component';
 
 @NgModule({
-  declarations: [ChannelCardComponent, VideoCardComponent, PlaylistCardComponent, LoadingComponent],
+  declarations: [
+    ChannelCardComponent,
+    VideoCardComponent,
+    PlaylistCardComponent,
+    LoadingComponent,
+    AvatarComponent,
+    InfoCardComponent,
+    CardThumbnailComponent],
   imports: [
     CommonModule
   ],
   exports: [
-    LoadingComponent
+    ChannelCardComponent,
+    VideoCardComponent,
+    PlaylistCardComponent,
+    LoadingComponent,
+    AvatarComponent,
+    InfoCardComponent,
+    CardThumbnailComponent
   ]
 })
 export class SharedModule { }
